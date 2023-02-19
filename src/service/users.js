62a731f1a62c6e7ff357dd4075a30062da20e6fc
Andrés Weitzel <andres96energy@hotmail.com>
2023-02-19 13:23:53 -0300

    Updated user service code doc

--- src/service/users.js
+++ src/service/users.js
@@ -6,9 +6,9 @@ let user;
 
 /**
  * @description gets all paged users with all their attributes
- * @param pageSizeNro Number type
- * @param pageNro Number type
- * @param orderBy Array Object type
+ * @param {Number} pageSizeNro Number type
+ * @param {Number} pageNro Number type
+ * @param {Object} orderBy Array Object type
  * @returns a list of paginated users
  * @example 
  * [{"id":1,"nickname":"RAFA-CON","first_name":"Rafael","last_name":"Castro","email":"rafael_castro88@gmail.com","identification_type":"DNI","identification_number":"445938822","country_id":"AR","creation_date":"2023-02-12T15:03:04.000Z","update_date":"2023-02-12T15:03:04.000Z"},{"id".....]
@@ -39,7 +39,7 @@ const getAll = async function (pageSizeNro, pageNro, orderBy) {
 
 /**
  * @description gets a user with all its attributes whose id matches the one passed as a parameter
- * @param id Number type
+ * @param {Number} id Number type
  * @returns a user according to his id
  * @example 
  * {"id":2,"nickname":"JAVIER GONZALEZ","first_name":"Javier","last_name":"Gonzalez","email":"javiBoquita@gmail.com","identification_type":"DNI","identification_number":"2672268765","country_id":"AR","creation_date":"2023-02-12T15:03:04.000Z","update_date":"2023-02-12T15:03:04.000Z"}
