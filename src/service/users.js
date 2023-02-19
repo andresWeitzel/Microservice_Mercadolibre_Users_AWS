d6a2f74a49068415ba194206e0507d6747f8fdb3
Andrés Weitzel <andres96energy@hotmail.com>
2023-02-19 13:20:56 -0300

    Updated user service code doc

--- src/service/users.js
+++ src/service/users.js
@@ -3,17 +3,24 @@ const { User } = require('../models/user');
 //Const/Vars
 let usersList;
 let user;
-const nroPage=0;
-const sizePage=5; 
 
-
-const getAll = async function () {
+/**
+ * @description gets all paged users with all their attributes
+ * @param pageSizeNro Number type
+ * @param pageNro Number type
+ * @param orderBy Array Object type
+ * @returns a list of paginated users
+ * @example 
+ * [{"id":1,"nickname":"RAFA-CON","first_name":"Rafael","last_name":"Castro","email":"rafael_castro88@gmail.com","identification_type":"DNI","identification_number":"445938822","country_id":"AR","creation_date":"2023-02-12T15:03:04.000Z","update_date":"2023-02-12T15:03:04.000Z"},{"id".....]
+ */
+const getAll = async function (pageSizeNro, pageNro, orderBy) {
     try {
         usersList = null;
         await User.findAll(
             {
-                limit: sizePage,
-                offset: nroPage,
+                limit: pageSizeNro,
+                offset: pageNro,
+                order: orderBy
             }
         )
             .then(users => {
@@ -30,6 +37,13 @@ const getAll = async function () {
 
 }
 
+/**
+ * @description gets a user with all its attributes whose id matches the one passed as a parameter
+ * @param id Number type
+ * @returns a user according to his id
+ * @example 
+ * {"id":2,"nickname":"JAVIER GONZALEZ","first_name":"Javier","last_name":"Gonzalez","email":"javiBoquita@gmail.com","identification_type":"DNI","identification_number":"2672268765","country_id":"AR","creation_date":"2023-02-12T15:03:04.000Z","update_date":"2023-02-12T15:03:04.000Z"}
+ */
 const getById = async function (id) {
     try {
         user = null;
