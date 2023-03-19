aa9fb8c6d6671d16cd22dfb9262732cd1cc7c40f
Andrés Weitzel <andres96energy@hotmail.com>
2023-03-18 21:59:31 -0300

    Updated and checked addUser logic

--- src/services/users/addUser.js
+++ src/services/users/addUser.js
@@ -3,7 +3,7 @@ const { Sequelize, Op } = require("sequelize");
 //Models
 const { User } = require('../../models/user');
 //Hepers
-const { currentDateTime,c } = require('../../helpers/dates/date');
+const { currentDateTime } = require('../../helpers/dates/date');
 const { checkDbAuthentication } = require("../../helpers/db/authenticate");
 //Const/Vars
 let user;
@@ -11,11 +11,23 @@ let dateNow;
 let checkDbConn;
 
 
-
+/**
+ * @description add user to database
+ * @param {String} nickname String type
+ * @param {String} firstName String type
+ * @param {String} lastName String type
+ * @param {String} email String type
+ * @param {String} identificationType String type
+ * @param {String} identificatioNumber String type  
+ * @param {String} countryId String type  
+ * @returns a json object with the new user
+ * @example
+ * {"id":null,"nickname":"JUANROMAN","first_name":"Juan","last_name":"Roman","email":"juan_roman@gmail.com","identification_type":"DNI","identification_number":"2221233",.....}
+ */
 const addUser = async function (nickname, firstName, lastName, email, identificationType, identificationNumber, countryId) {
     try {
         user = null;
-        dateNow = await c();
+        dateNow = await currentDateTime();
 
         checkDbConn = await checkDbAuthentication();
 
