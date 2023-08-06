f3d0fcf6845633300eed8d033aa6a6a77e2bccbe
Andrés Weitzel <andres96energy@hotmail.com>
2023-08-06 13:25:52 -0300

    For pr

--- src/controllers/users/adduser.js
+++ src/controllers/users/adduser.js
@@ -1,32 +1,25 @@
 "use strict";
 //Services
-const {
-  addUser
-} = require("../../services/users/addUser");
+const { addUser } = require("../../services/users/addUser");
+//Models
+const { UserDto } = require("../../models/dto/user-dto");
 //Enums
-const {
-  statusCode
-} = require("../../enums/http/statusCode");
-const {
-  statusName
-} = require("../../enums/connection/statusName");
-const {
-  value
-} = require("../../enums/general/value");
+const { statusCode } = require("../../enums/http/statusCode");
+const { statusName } = require("../../enums/connection/statusName");
+const { value } = require("../../enums/general/value");
 //Helpers
-const {
-  requestResult
-} = require("../../helpers/http/bodyResponse");
+const { requestResult } = require("../../helpers/http/bodyResponse");
 const {
   validateHeadersParams,
 } = require("../../helpers/http/requestHeadersParams");
 const {
+  validateClass,
+} = require("../../helpers/class-validator/validate-properties");
+const {
   validateBodyAddUserParams,
 } = require("../../helpers/http/users/requestBodyAddUserParams");
-const {
-  validateAuthHeaders
-} = require("../../helpers/auth/headers");
-
+const { validateAuthHeaders } = require("../../helpers/auth/headers");
+const { currentDateTime } = require("../../helpers/dates/date");
 
 //Const/Vars
 let newUser;
@@ -41,6 +34,7 @@ let lastName;
 let email;
 let identType;
 let identNumber;
+let dateNow;
 let countryId;
 let msg;
 let code;
@@ -82,59 +76,82 @@ module.exports.handler = async (event) => {
     }
     //-- end with validation Headers  ---
 
-    //-- start with validation Body  ---
+    // //-- start with validation Body  ---
 
     eventBody = JSON.parse(await event.body);
 
-    validateReqBodyParams = await validateBodyAddUserParams(eventBody);
+    // validateReqBodyParams = await validateBodyAddUserParams(eventBody);
 
-    if (!validateReqBodyParams) {
-      return await requestResult(
-        statusCode.BAD_REQUEST,
-        "Bad request, check request attributes. Missing or incorrect. CHECK: nickname, first_name and last_name (required|string|minLength:4|maxLength:50), email (required|string|minLength:10|maxLength:100), identification_type and identification_number (required|string|minLength:6|maxLength:20), country_id (required|string|minLength:2|maxLength:5)"
-      );
-    }
-    //-- end with validation Body  ---
+    // if (!validateReqBodyParams) {
+    //   return await requestResult(
+    //     statusCode.BAD_REQUEST,
+    //     "Bad request, check request attributes. Missing or incorrect. CHECK: nickname, first_name and last_name (required|string|minLength:4|maxLength:50), email (required|string|minLength:10|maxLength:100), identification_type and identification_number (required|string|minLength:6|maxLength:20), country_id (required|string|minLength:2|maxLength:5)"
+    //   );
+    // }
+    // //-- end with validation Body  ---
 
     //-- start with db query  ---
 
-    nickname = eventBody.nickname;
-    firstName = eventBody.first_name;
-    lastName = eventBody.last_name;
-    email = eventBody.email;
-    identType = eventBody.identification_type;
-    identNumber = eventBody.identification_number;
-    countryId = eventBody.country_id;
+    nickname = eventBody?.nickname;
+    firstName = eventBody?.first_name;
+    lastName = eventBody?.last_name;
+    email = eventBody?.email;
+    identType = eventBody?.identification_type;
+    identNumber = eventBody?.identification_number;
+    countryId = eventBody?.country_id;
+    dateNow = await currentDateTime();
+    creationDate=dateNow;
+    updateDate=dateNow;
 
-    newUser = await addUser(
+
+    UserDto;
+
+    newUser = new UserDto(
       nickname,
       firstName,
       lastName,
       email,
       identType,
       identNumber,
-      countryId
+      countryId,
+      creationDate,
+      updateDate
     );
-    
-    switch (newUser) {
-      case statusName.CONNECTION_REFUSED:
-        return await requestResult(
-          statusCode.INTERNAL_SERVER_ERROR,
-          "ECONNREFUSED. An error has occurred with the connection or query to the database. CHECK: The first_name next together the last_name should be uniques. The identification_type next together the identification_number should be uniques."
-        );
-      case statusName.CONNECTION_ERROR:
-        return await requestResult(
-          statusCode.INTERNAL_SERVER_ERROR,
-          "ERROR. An error has occurred in the process operations and queries with the database Caused by SequelizeConnectionRefusedError: connect ECONNREFUSED 127.0.0.1:3306."
-        );
-      case value.IS_ZERO_NUMBER || value.IS_UNDEFINED || value.IS_NULL:
-        return await requestResult(
-          statusCode.BAD_REQUEST,
-          "Bad request, could not add user.CHECK: The first_name next together the last_name should be uniques. The identification_type next together the identification_number should be uniques."
-        );
-      default:
-        return await requestResult(statusCode.OK, newUser);
+
+    //-- start with validation object  ---
+    validateObject = await validateClass(newUser);
+
+    if (validateObject.length) {
+      return await requestResult(
+        statusCode.BAD_REQUEST,
+        `Bad request, check request attributes. Validate the following : ${validateObject}`
+      );
     }
+    return await requestResult(
+      statusCode.OK,
+      newUser
+    );
+    //-- end with validation object  ---
+
+    // switch (newUser) {
+    //   case statusName.CONNECTION_REFUSED:
+    //     return await requestResult(
+    //       statusCode.INTERNAL_SERVER_ERROR,
+    //       "ECONNREFUSED. An error has occurred with the connection or query to the database. CHECK: The first_name next together the last_name should be uniques. The identification_type next together the identification_number should be uniques."
+    //     );
+    //   case statusName.CONNECTION_ERROR:
+    //     return await requestResult(
+    //       statusCode.INTERNAL_SERVER_ERROR,
+    //       "ERROR. An error has occurred in the process operations and queries with the database Caused by SequelizeConnectionRefusedError: connect ECONNREFUSED 127.0.0.1:3306."
+    //     );
+    //   case value.IS_ZERO_NUMBER || value.IS_UNDEFINED || value.IS_NULL:
+    //     return await requestResult(
+    //       statusCode.BAD_REQUEST,
+    //       "Bad request, could not add user.CHECK: The first_name next together the last_name should be uniques. The identification_type next together the identification_number should be uniques."
+    //     );
+    //   default:
+    //     return await requestResult(statusCode.OK, newUser);
+    // }
     //-- end with db query  ---
   } catch (error) {
     msg = `Error in addUser lambda. Caused by ${error}`;
---
+++ src/models/dto/user-dto.js
@@ -0,0 +1,92 @@
+//External
+import { IsNotEmpty, IsString, IsEmail, Length, IsDate } from "class-validator";
+//Vars-const
+const MIN_VALUE_NICKNAME = 4;
+const MAX_VALUE_NICKNAME = 50;
+const MIN_VALUE_FIRST_LAST_NAME = 4;
+const MAX_VALUE_FIRST_LAST_NAME = 70;
+const MIN_VALUE_EMAIL = 4;
+const MAX_VALUE_EMAIL = 100;
+const MIN_VALUE_IDENT_TYPE = 2;
+const MAX_VALUE_IDENT_TYPE = 20;
+const MIN_VALUE_IDENT_NUMBER = 2;
+const MAX_VALUE_IDENT_NUMBER = 50;
+const MIN_VALUE_COUNTRY_ID = 1;
+const MAX_VALUE_COUNTRY_ID = 10;
+
+/**
+ * @description model class for users
+ * @property {string} nickname string type.
+ * @property {string} first_name string type.
+ * @property {string} last_name string type.
+ * @property {string} email string type.
+ * @property {string} identification_type string type.
+ * @property {string} identification_number string type.
+ * @property {string} country_id string type.
+ * @property {Date} creation_date Date type.
+ * @property {Date} update_date Date type.
+ */
+class UserDto {
+  @IsNotEmpty({ message: "The nickname cannot be empty" })
+  @IsString({ message: "The nickname must be of type string" })
+  @Length(MIN_VALUE_NICKNAME, MAX_VALUE_NICKNAME, {
+    message: `The value of the nickname must be between ${MIN_VALUE_NICKNAME} and ${MAX_VALUE_NICKNAME} characters`,
+  })
+  nickname;
+
+  @IsNotEmpty({ message: "The first_name cannot be empty" })
+  @IsString({ message: "The first_name must be of type string" })
+  @IsEmail()
+  @Length(MIN_VALUE_FIRST_LAST_NAME, MAX_VALUE_FIRST_LAST_NAME, {
+    message: `The value of the first_name must be between ${MIN_VALUE_FIRST_LAST_NAME} and ${MAX_VALUE_FIRST_LAST_NAME} characters`,
+  })
+  first_name;
+
+  @IsNotEmpty({ message: "The last_name cannot be empty" })
+  @IsString({ message: "The last_name must be of type string" })
+  @Length(MIN_VALUE_FIRST_LAST_NAME, MAX_VALUE_FIRST_LAST_NAME, {
+    message: `The value of the last_name must be between ${MIN_VALUE_FIRST_LAST_NAME} and ${MAX_VALUE_FIRST_LAST_NAME} characters`,
+  })
+  last_name;
+
+  @IsNotEmpty({ message: "The email cannot be empty" })
+  @IsEmail()
+  @IsString({ message: "The email must be of type string" })
+  @Length(MIN_VALUE_EMAIL, MAX_VALUE_EMAIL, {
+    message: `The value of the email must be between ${MIN_VALUE_EMAIL} and ${MAX_VALUE_EMAIL} characters`,
+  })
+  email;
+
+  @IsNotEmpty({ message: "The identification_type cannot be empty" })
+  @IsString({ message: "The identification_type must be of type string" })
+  @Length(MIN_VALUE_IDENT_TYPE, MAX_VALUE_IDENT_TYPE, {
+    message: `The value of the identification_type must be between ${MIN_VALUE_IDENT_TYPE} and ${MAX_VALUE_IDENT_TYPE} characters`,
+  })
+  identification_type;
+
+  @IsNotEmpty({ message: "The identification_number cannot be empty" })
+  @IsString({ message: "The identification_number must be of type string" })
+  @Length(MIN_VALUE_IDENT_NUMBER, MAX_VALUE_IDENT_NUMBER, {
+    message: `The value of the identification_number must be between ${MIN_VALUE_IDENT_NUMBER} and ${MAX_VALUE_IDENT_NUMBER} characters`,
+  })
+  identification_number;
+
+  @IsNotEmpty({ message: "The country_id cannot be empty" })
+  @IsString({ message: "The country_id must be of type string" })
+  @Length(MIN_VALUE_COUNTRY_ID, MAX_VALUE_COUNTRY_ID, {
+    message: `The value of the country_id must be between ${MIN_VALUE_COUNTRY_ID} and ${MAX_VALUE_COUNTRY_ID} characters`,
+  })
+  country_id;
+
+  @IsNotEmpty({ message: "The creation_date cannot be empty" })
+  @IsDate({ message: "The creation_date must be of type Date" })
+  creation_date;
+
+  @IsNotEmpty({ message: "The update_date cannot be empty" })
+  @IsDate({ message: "The update_date must be of type Date" })
+  update_date;
+}
+
+module.exports = {
+  UserDto,
+};
