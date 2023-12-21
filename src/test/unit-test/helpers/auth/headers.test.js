3b4ab04700a2931d7e7b6302b14d79b1a41db9f5
Andrés Weitzel <andres96energy@hotmail.com>
2023-12-20 23:38:56 -0300

    Added test

--- src/test/unit-test/helpers/auth/headers.test.js
+++ src/test/unit-test/helpers/auth/headers.test.js
@@ -1,139 +1,70 @@
 "use strict";
-//Enums
-const { statusCode, statusCodeDetails } = require("../../../../enums/http/status-code");
 //Helpers
 const { validateAuthHeaders } = require("../../../../helpers/auth/headers");
+//Environment vars
+const X_API_KEY = process.env.X_API_KEY;
 //Const
-const OK_CODE = statusCode.OK;
-const OK_CODE_DETAILS = statusCodeDetails.OK;
-const BAD_REQUEST_CODE = statusCode.BAD_REQUEST;
-const BAD_REQUEST_CODE_DETAILS = statusCodeDetails.BAD_REQUEST_CODE_DETAILS;
 const EVENT_HEADERS_MOCK = {};
+const EVENT_HEADERS_WITH_API_KEY_INVALID = {
+  'x-api-key' : 'f98d8cd98h73s204e3456998ecknaksdnakndkn'
+};
+const EVENT_HEADERS_WITH_API_KEY_VALID = {
+  'x-api-key' : X_API_KEY
+};
+const EVENT_HEADERS_WITH_API_KEY_AUTH = {
+  'x-api-key' : 'f9hjdbab32b1nb1n2204e3456998ecl9427j',
+  'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+};
 //Vars
 let msg;
 let validateAuthHeadersResult;
 
+
 describe("- validateAuthHeaders helper (Unit Test)", () => {
   describe("1) Check cases for argument.", () => {
-    msg = "Should return a boolean passing values to all parameters.";
+    msg = "Should return a boolean if passed values to all parameters.";
     it(msg, async () => {
       validateAuthHeadersResult = await validateAuthHeaders(EVENT_HEADERS_MOCK);
       await expect(typeof validateAuthHeadersResult == "boolean").toBe(true);
     });
 
-    msg = "Should return a boolean passing other values.";
+    msg = "Should return a boolean if passed other values.";
     it(msg, async () => {
       validateAuthHeadersResult = await validateAuthHeaders(EVENT_HEADERS_MOCK, EVENT_HEADERS_MOCK);
       await expect(typeof validateAuthHeadersResult == "boolean").toBe(true);
     });
 
-    // msg =
-    //   "Should return an object with the same values of the status code and the message passed as a parameter.";
+    msg = "Should return a boolean if a null value is passed to the function.";
+    it(msg, async () => {
+      validateAuthHeadersResult = await validateAuthHeaders(null);
+      await expect(typeof validateAuthHeadersResult == "boolean").toBe(true);
+    });
 
-    // it(msg, async () => {
-    //   validateAuthHeadersResult = await validateAuthHeaders(OK_CODE, OK_CODE_DETAILS);
-    //   expect(typeof validateAuthHeadersResult == "object").toBe(true);
+    msg = "Should return a boolean if a undefined value is passed to the function.";
+    it(msg, async () => {
+      validateAuthHeadersResult = await validateAuthHeaders(undefined);
+      await expect(typeof validateAuthHeadersResult == "boolean").toBe(true);
+    });
 
-    //   expect(validateAuthHeadersResult.statusCode == OK_CODE).toBe(true);
-
-    //   let bodyConversion = JSON.parse(validateAuthHeadersResult.body);
-
-    //   let bodyConversionMessage = JSON.stringify(bodyConversion.message);
-
-    //   expect(bodyConversionMessage == JSON.stringify(OK_CODE_DETAILS)).toBe(
-    //     true
-    //   );
-    // });
-
-    // msg =
-    //   "Should return an object with the value of the statusCode parameter of type any (Number or String)";
-
-    // it(msg, async () => {
-    //   validateAuthHeadersResult = await validateAuthHeaders(
-    //     BAD_REQUEST_CODE,
-    //     BAD_REQUEST_CODE_DETAILS
-    //   );
-
-    //   expect(typeof validateAuthHeadersResult == "object").toBe(true);
-
-    //   expect(
-    //     typeof validateAuthHeadersResult.statusCode == "number" ||
-    //       typeof validateAuthHeadersResult.statusCode == "string"
-    //   ).toBe(true);
-    // });
-
-    // msg =
-    //   "Should return an object with the value of the body parameter of type string";
-
-    // it(msg, async () => {
-    //   validateAuthHeadersResult = await validateAuthHeaders(
-    //     BAD_REQUEST_CODE,
-    //     BAD_REQUEST_CODE_DETAILS
-    //   );
-
-    //   expect(typeof validateAuthHeadersResult == "object").toBe(true);
-
-    //   expect(typeof validateAuthHeadersResult.body == "string").toBe(true);
-    // });
-
-    // msg = "Should return an object if no parameters are passed.";
-
-    // it(msg, async () => {
-    //   validateAuthHeadersResult = await validateAuthHeaders();
-
-    //   expect(typeof validateAuthHeadersResult == "object").toBe(true);
-    // });
-
-    // msg =
-    //   "Should return an object with the value undefined for statusCode and '{}' for body if no parameters are passed.";
-
-    // it(msg, async () => {
-    //   validateAuthHeadersResult = await validateAuthHeaders();
-
-    //   expect(typeof validateAuthHeadersResult == "object").toBe(true);
-
-    //   expect(validateAuthHeadersResult.statusCode == undefined).toBe(true);
-
-    //   expect(validateAuthHeadersResult.body == "{}").toBe(true);
-    // });
-
-    // msg =
-    //   "Should return an object with the value null for statusCode if null is passed as a parameter for statusCode argument.";
-
-    // it(msg, async () => {
-    //   validateAuthHeadersResult = await validateAuthHeaders(null);
-
-    //   expect(typeof validateAuthHeadersResult == "object").toBe(true);
-
-    //   expect(validateAuthHeadersResult.statusCode == null).toBe(true);
-    // });
-
-    // msg =
-    //   "Should return an object with string format for body if null is passed as a parameter for message argument.";
-
-    // it(msg, async () => {
-    //   validateAuthHeadersResult = await validateAuthHeaders(BAD_REQUEST_CODE, null);
-
-    //   expect(typeof validateAuthHeadersResult == "object").toBe(true);
-
-    //   expect(typeof validateAuthHeadersResult.body == "string").toBe(true);
-    // });
-
-    // msg =
-    //   "Should return an object with the value undefined for statusCode if a value is not passed as a parameter for statusCode argument.";
-
-    // it(msg, async () => {
-    //   validateAuthHeadersResult = await validateAuthHeaders();
-
-    //   expect(typeof validateAuthHeadersResult == "object").toBe(true);
-
-    //   expect(validateAuthHeadersResult.statusCode == undefined).toBe(true);
-    // });
+    msg = "Should return a boolean if not value is passed to the function.";
+    it(msg, async () => {
+      validateAuthHeadersResult = await validateAuthHeaders();
+      await expect(typeof validateAuthHeadersResult == "boolean").toBe(true);
+    });
   });
-//   describe("2) Check cases for error.", () => {
-//     msg = "Should return a boolean with value false if a new Error is passed";
-//     it(msg, async () => {
-//       await expect(async() => await validateAuthHeaders(new Error())).not.toThrow(Error);
-//     });
-//   });
+
+  // describe("2) Check cases for event headers .", () => {
+  //   msg = "Should return a boolean with true value if a correc x-api-key value is passed to the function.";
+  //   it(msg, async () => {
+  //     validateAuthHeadersResult = await validateAuthHeaders(EVENT_HEADERS_WITH_API_KEY_VALID);
+  //     await expect(validateAuthHeadersResult == true).toBe(true);
+  //   });
+
+  //   msg = "Should return a boolean with true false if an incorrect x-api-key value is passed to the function.";
+  //   it(msg, async () => {
+  //     validateAuthHeadersResult = await validateAuthHeaders(EVENT_HEADERS_WITH_API_KEY_INVALID);
+  //     await expect(validateAuthHeadersResult == false).toBe(true);
+  //   });
+  // });
+
 });
