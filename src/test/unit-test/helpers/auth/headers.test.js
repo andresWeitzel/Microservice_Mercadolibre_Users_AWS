8bc078ec5b2ea7b3d10490bef9b1918576c46f2a
Andrés Weitzel <andres96energy@hotmail.com>
2024-01-18 18:37:59 -0300

    Added test

--- src/test/unit-test/helpers/auth/headers.test.js
+++ src/test/unit-test/helpers/auth/headers.test.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 //Helpers
-const { validateAuthHeaders } = require("../../../../helpers/auth/headers");
+const { validateAuthHeaders } = require('../../../../helpers/auth/headers');
 //Environment vars
-const MOCK_VALID_X_API_KEY = process.env.X_API_KEY;
-const MOCK_INVALID_X_API_KEY = process.env.MOCK_INVALID_X_API_KEY;
+const MOCK_VALID_X_API_KEY_01 = process.env.X_API_KEY;
+const MOCK_INVALID_X_API_KEY_01 = process.env.MOCK_INVALID_X_API_KEY_01;
 const MOCK_VALID_AUTHORIZATION = process.env.BEARER_TOKEN;
 const MOCK_INVALID_BEARER_TOKEN_01 = process.env.MOCK_INVALID_BEARER_TOKEN_01;
 const MOCK_INVALID_BEARER_TOKEN_02 = process.env.MOCK_INVALID_BEARER_TOKEN_02;
@@ -11,110 +11,108 @@ const MOCK_INVALID_BEARER_TOKEN_02 = process.env.MOCK_INVALID_BEARER_TOKEN_02;
 const MOCK_EVENT_HEADERS = {};
 const MOCK_EVENT_HEADERS_WITH_API_KEY_AUTH_INVALID = {
   headers: {
-    "x-api-key": MOCK_INVALID_X_API_KEY,
+    'x-api-key': MOCK_INVALID_X_API_KEY_01,
     Authorization: MOCK_INVALID_BEARER_TOKEN_01,
   },
 };
 const EVENT_HEADERS_WITH_API_KEY_AUTH_VALID = {
   headers: {
-    "x-api-key": MOCK_VALID_X_API_KEY,
+    'x-api-key': MOCK_VALID_X_API_KEY_01,
     Authorization: MOCK_VALID_AUTHORIZATION,
   },
 };
-
 const EVENT_HEADERS_WITH_API_KEY_VALID_AUTH_INVALID = {
   headers: {
-    "x-api-key": MOCK_VALID_X_API_KEY,
+    'x-api-key': MOCK_VALID_X_API_KEY_01,
     Authorization: MOCK_INVALID_BEARER_TOKEN_02,
   },
 };
-
 //Vars
 let msg;
 let validateAuthHeadersResult;
 
-describe("- validateAuthHeaders helper (Unit Test)", () => {
-  describe("1) Check cases for argument.", () => {
-    msg = "Should return a boolean if passed values to all parameters.";
+describe('- validateAuthHeaders helper (Unit Test)', () => {
+  describe('1) Check cases for argument.', () => {
+    msg = 'Should return a boolean if passed values to all parameters.';
     it(msg, async () => {
       validateAuthHeadersResult = await validateAuthHeaders(MOCK_EVENT_HEADERS);
-      await expect(typeof validateAuthHeadersResult == "boolean").toBe(true);
+      await expect(typeof validateAuthHeadersResult == 'boolean').toBe(true);
     });
 
-    msg = "Should return a boolean if passed other values.";
+    msg = 'Should return a boolean if passed other values.';
     it(msg, async () => {
       validateAuthHeadersResult = await validateAuthHeaders(
         MOCK_EVENT_HEADERS,
-        MOCK_EVENT_HEADERS
+        MOCK_EVENT_HEADERS,
       );
-      await expect(typeof validateAuthHeadersResult == "boolean").toBe(true);
+      await expect(typeof validateAuthHeadersResult == 'boolean').toBe(true);
     });
 
-    msg = "Should return a string if a null value is passed to the function.";
+    msg = 'Should return a string if a null value is passed to the function.';
     it(msg, async () => {
       validateAuthHeadersResult = await validateAuthHeaders(null);
-      await expect(typeof validateAuthHeadersResult == "string").toBe(true);
+      await expect(typeof validateAuthHeadersResult == 'string').toBe(true);
     });
 
     msg =
-      "Should return a string if a undefined value is passed to the function.";
+      'Should return a string if a undefined value is passed to the function.';
     it(msg, async () => {
       validateAuthHeadersResult = await validateAuthHeaders(undefined);
-      await expect(typeof validateAuthHeadersResult == "string").toBe(true);
+      await expect(typeof validateAuthHeadersResult == 'string').toBe(true);
     });
 
-    msg = "Should return a string if not value is passed to the function.";
+    msg = 'Should return a string if not value is passed to the function.';
     it(msg, async () => {
       validateAuthHeadersResult = await validateAuthHeaders();
-      await expect(typeof validateAuthHeadersResult == "string").toBe(true);
+      await expect(typeof validateAuthHeadersResult == 'string').toBe(true);
     });
   });
 
-  describe("2) Check cases for event headers .", () => {
+  describe('2) Check cases for event headers .', () => {
     msg =
-      "Should return a boolean with true value if a correct x-api-key and MOCK_VALID_authorization value is passed to the function.";
+      'Should return a boolean with true value if a correct x-api-key and MOCK_VALID_authorization value is passed to the function.';
     it(msg, async () => {
       validateAuthHeadersResult = await validateAuthHeaders(
-        EVENT_HEADERS_WITH_API_KEY_AUTH_VALID
+        EVENT_HEADERS_WITH_API_KEY_AUTH_VALID,
       );
       await expect(validateAuthHeadersResult == true).toBe(true);
     });
 
     msg =
-      "Should return a boolean with false value if a correct x-api-key and incorrect MOCK_VALID_authorization value is passed to the function.";
+      'Should return a boolean with false value if a correct x-api-key and incorrect MOCK_VALID_authorization value is passed to the function.';
     it(msg, async () => {
       validateAuthHeadersResult = await validateAuthHeaders(
-        EVENT_HEADERS_WITH_API_KEY_VALID_AUTH_INVALID
+        EVENT_HEADERS_WITH_API_KEY_VALID_AUTH_INVALID,
       );
       await expect(validateAuthHeadersResult == false).toBe(true);
     });
 
     msg =
-      "Should return a boolean with false value if a invalid x-api-key and MOCK_VALID_authorization value is passed to the function.";
+      'Should return a boolean with false value if a invalid x-api-key and MOCK_VALID_authorization value is passed to the function.';
     it(msg, async () => {
       validateAuthHeadersResult = await validateAuthHeaders(
-        MOCK_EVENT_HEADERS_WITH_API_KEY_AUTH_INVALID
+        MOCK_EVENT_HEADERS_WITH_API_KEY_AUTH_INVALID,
       );
       await expect(validateAuthHeadersResult == false).toBe(true);
     });
   });
 
-  describe("3) Check cases for error.", () => {
-    msg = "Should not throw an error if a new Error() is passed as a parameter";
+  describe('3) Check cases for error.', () => {
+    msg = 'Should not throw an error if a new Error() is passed as a parameter';
 
     it(msg, async () => {
       await expect(
-        async () => await validateAuthHeaders(new Error())
+        async () => await validateAuthHeaders(new Error()),
       ).not.toThrow(Error);
     });
 
     msg =
-      "Should return a boolean with false value if a new Error() value is passed";
+      'Should return a boolean with false value if a new Error() value is passed';
 
     it(msg, async () => {
       validateAuthHeadersResult = await validateAuthHeaders(new Error());
 
-      await expect(typeof validateAuthHeadersResult == "boolean").toBe(true);
+      await expect(typeof validateAuthHeadersResult == 'boolean').toBe(true);
 
       await expect(validateAuthHeadersResult == false).toBe(true);
     });
--- src/test/unit-test/helpers/http/body-response.test.js
+++ src/test/unit-test/helpers/http/body-response.test.js
@@ -1,36 +1,35 @@
-"use strict";
+'use strict';
 //Helpers
-const { requestResult } = require("../../../../helpers/http/body-response");
+const { requestResult } = require('../../../../helpers/http/body-response');
 //Enums
 const {
   statusCode,
   statusCodeDetails,
-} = require("../../../../enums/http/status-code");
+} = require('../../../../enums/http/status-code');
 //Const
 const OK_CODE = statusCode.OK;
 const OK_CODE_DETAILS = statusCodeDetails.OK;
 const BAD_REQUEST_CODE = statusCode.BAD_REQUEST;
 const BAD_REQUEST_CODE_DETAILS = statusCodeDetails.BAD_REQUEST_CODE_DETAILS;
-const MOCK_OBJECT = {};
 //Vars
 let msg;
 let requestResultResult;
 
-describe("- requestResult helper (Unit test)", () => {
+describe('- requestResult helper (Unit test)', () => {
   //--Start first suite --
-  describe("1) Check cases for arguments.", () => {
-    msg = "Should return an object passing values to all parameters.";
+  describe('1) Check cases for arguments.', () => {
+    msg = 'Should return an object passing values to all parameters.';
     it(msg, async () => {
       requestResultResult = await requestResult(OK_CODE, OK_CODE_DETAILS);
-      await expect(typeof requestResultResult == "object").toBe(true);
+      await expect(typeof requestResultResult == 'object').toBe(true);
     });
 
     msg =
-      "Should return an object with the same values of the status code and the message passed as a parameter.";
+      'Should return an object with the same values of the status code and the message passed as a parameter.';
 
     it(msg, async () => {
       requestResultResult = await requestResult(OK_CODE, OK_CODE_DETAILS);
-      expect(typeof requestResultResult == "object").toBe(true);
+      expect(typeof requestResultResult == 'object').toBe(true);
 
       expect(requestResultResult.statusCode == OK_CODE).toBe(true);
 
@@ -39,47 +38,47 @@ describe("- requestResult helper (Unit test)", () => {
       let bodyConversionMessage = JSON.stringify(bodyConversion.message);
 
       expect(bodyConversionMessage == JSON.stringify(OK_CODE_DETAILS)).toBe(
-        true
+        true,
       );
     });
 
     msg =
-      "Should return an object with the value of the statusCode parameter of type any (Number or String)";
+      'Should return an object with the value of the statusCode parameter of type any (Number or String)';
 
     it(msg, async () => {
       requestResultResult = await requestResult(
         BAD_REQUEST_CODE,
-        BAD_REQUEST_CODE_DETAILS
+        BAD_REQUEST_CODE_DETAILS,
       );
 
-      expect(typeof requestResultResult == "object").toBe(true);
+      expect(typeof requestResultResult == 'object').toBe(true);
 
       expect(
-        typeof requestResultResult.statusCode == "number" ||
-          typeof requestResultResult.statusCode == "string"
+        typeof requestResultResult.statusCode == 'number' ||
+          typeof requestResultResult.statusCode == 'string',
       ).toBe(true);
     });
 
     msg =
-      "Should return an object with the value of the body parameter of type string";
+      'Should return an object with the value of the body parameter of type string';
 
     it(msg, async () => {
       requestResultResult = await requestResult(
         BAD_REQUEST_CODE,
-        BAD_REQUEST_CODE_DETAILS
+        BAD_REQUEST_CODE_DETAILS,
       );
 
-      expect(typeof requestResultResult == "object").toBe(true);
+      expect(typeof requestResultResult == 'object').toBe(true);
 
-      expect(typeof requestResultResult.body == "string").toBe(true);
+      expect(typeof requestResultResult.body == 'string').toBe(true);
     });
 
-    msg = "Should return an object if no parameters are passed.";
+    msg = 'Should return an object if no parameters are passed.';
 
     it(msg, async () => {
       requestResultResult = await requestResult();
 
-      expect(typeof requestResultResult == "object").toBe(true);
+      expect(typeof requestResultResult == 'object').toBe(true);
     });
 
     msg =
@@ -88,54 +87,54 @@ describe("- requestResult helper (Unit test)", () => {
     it(msg, async () => {
       requestResultResult = await requestResult();
 
-      expect(typeof requestResultResult == "object").toBe(true);
+      expect(typeof requestResultResult == 'object').toBe(true);
 
       expect(requestResultResult.statusCode == undefined).toBe(true);
 
-      expect(requestResultResult.body == "{}").toBe(true);
+      expect(requestResultResult.body == '{}').toBe(true);
     });
 
     msg =
-      "Should return an object with the value null for statusCode if null is passed as a parameter for statusCode argument.";
+      'Should return an object with the value null for statusCode if null is passed as a parameter for statusCode argument.';
 
     it(msg, async () => {
       requestResultResult = await requestResult(null);
 
-      expect(typeof requestResultResult == "object").toBe(true);
+      expect(typeof requestResultResult == 'object').toBe(true);
 
       expect(requestResultResult.statusCode == null).toBe(true);
     });
 
     msg =
-      "Should return an object with string format for body if null is passed as a parameter for message argument.";
+      'Should return an object with string format for body if null is passed as a parameter for message argument.';
 
     it(msg, async () => {
       requestResultResult = await requestResult(BAD_REQUEST_CODE, null);
 
       console.log(typeof requestResultResult.body);
 
-      expect(typeof requestResultResult == "object").toBe(true);
+      expect(typeof requestResultResult == 'object').toBe(true);
 
-      expect(typeof requestResultResult.body == "string").toBe(true);
+      expect(typeof requestResultResult.body == 'string').toBe(true);
     });
 
     msg =
-      "Should return an object with the value undefined for statusCode if a value is not passed as a parameter for statusCode argument.";
+      'Should return an object with the value undefined for statusCode if a value is not passed as a parameter for statusCode argument.';
 
     it(msg, async () => {
       requestResultResult = await requestResult();
 
-      expect(typeof requestResultResult == "object").toBe(true);
+      expect(typeof requestResultResult == 'object').toBe(true);
 
       expect(requestResultResult.statusCode == undefined).toBe(true);
     });
   });
 
-  describe("2) Check cases for error.", () => {
-    msg = "Should return a boolean with value false if an new Error is passed";
+  describe('2) Check cases for error.', () => {
+    msg = 'Should return a boolean with value false if an new Error is passed';
     it(msg, async () => {
       await expect(async () => await requestResult(new Error())).not.toThrow(
-        Error
+        Error,
       );
     });
   });
