7c4f77cbd239a057bb79fdb12f63dd5af772620d
Andrés Weitzel <andres96energy@hotmail.com>
2024-01-26 17:51:52 -0300

    Updated jest config

--- src/test/mock/set-env-vars.js
+++ src/test/mock/set-env-vars.js
@@ -1,5 +1,7 @@
 // Mock Testing
 process.env.MOCK_BOOLEAN_VALUE_01 = false;
+process.env.MOCK_NUMBER_VALUE_01 = 16287426;
+process.env.MOCK_STRING_VALUE_01 = "MOCK_STRING";
 process.env.MOCK_OBJECT_VALUE_01 = 'TEST_01';
 process.env.MOCK_OBJECT_VALUE_02 = 'TEST_02';
 process.env.MOCK_VALID_OBJECT_01 = '12315236751236516235';
--- src/test/unit-test/helpers/http/query-string-params.test.js
+++ src/test/unit-test/helpers/http/query-string-params.test.js
@@ -6,16 +6,19 @@ const {
   statusCode,
   statusCodeDetails,
 } = require("../../../../enums/http/status-code");
-const { validatePathParameters } = require("../../../../helpers/http/query-string-params");
+const {
+  validatePathParameters,
+} = require("../../../../helpers/http/query-string-params");
 //Const
 const OK_CODE = statusCode.OK;
 const OK_CODE_DETAILS = statusCodeDetails.OK;
 const BAD_REQUEST_CODE = statusCode.BAD_REQUEST;
 const BAD_REQUEST_CODE_DETAILS = statusCodeDetails.BAD_REQUEST_CODE_DETAILS;
-const MOCK_OBJECT= {mock_object:"MOCK_OBJECT"};
-const MOCK_NUMBER= 12313;
-const MOCK_STRING= "MOCK_STRING";
-const MOCK_BOOLEAN= true;
+const MOCK_OBJECT_VALUE_01 = process.env.MOCK_OBJECT_VALUE_01;
+const MOCK_OBJECT = { mock_object: MOCK_OBJECT_VALUE_01 };
+const MOCK_NUMBER = process.env.MOCK_NUMBER_VALUE_01;
+const MOCK_STRING = process.env.MOCK_STRING_VALUE_01;
+const MOCK_BOOLEAN = process.env.MOCK_BOOLEAN_VALUE_01;
 //Vars
 let msg;
 let validatePathParametersResult;
@@ -23,143 +26,62 @@ let validatePathParametersResult;
 describe("- validatePathParameters helper (Unit test)", () => {
   //--Start first suite --
   describe("1) Check cases for arguments.", () => {
-    msg = "Should return a boolean value if object type argument is passed ( This function expects one argument of type object ).";
+    msg =
+      "Should return a boolean value if object type argument is passed ( This function expects one argument of type object ).";
     it(msg, async () => {
       validatePathParametersResult = await validatePathParameters(MOCK_OBJECT);
       await expect(typeof validatePathParametersResult == "boolean").toBe(true);
     });
 
-    msg = "Should return a boolean value if number type argument is passed ( This function expects one argument of type object ).";
+    msg =
+      "Should return a boolean value if number type argument is passed ( This function expects one argument of type object ).";
     it(msg, async () => {
       validatePathParametersResult = await validatePathParameters(MOCK_NUMBER);
       await expect(typeof validatePathParametersResult == "boolean").toBe(true);
     });
 
-    
-    msg = "Should return a boolean value if a string type argument is passed ( This function expects one argument of type object ).";
+    msg =
+      "Should return a boolean value if a string type argument is passed ( This function expects one argument of type object ).";
     it(msg, async () => {
       validatePathParametersResult = await validatePathParameters(MOCK_STRING);
       await expect(typeof validatePathParametersResult == "boolean").toBe(true);
     });
 
-    msg = "Should return a boolean value if a boolean type argument is passed ( This function expects one argument of type object ).";
+    msg =
+      "Should return a boolean value if a boolean type argument is passed ( This function expects one argument of type object ).";
     it(msg, async () => {
       validatePathParametersResult = await validatePathParameters(MOCK_BOOLEAN);
       await expect(typeof validatePathParametersResult == "boolean").toBe(true);
     });
 
-    // msg =
-    //   "Should return an object with the same values of the status code and the message passed as a parameter.";
+    msg =
+      "Should return a boolean value if a null argument is passed ( This function expects one argument of type object ).";
+    it(msg, async () => {
+      validatePathParametersResult = await validatePathParameters(null);
+      await expect(typeof validatePathParametersResult == "boolean").toBe(true);
+    });
 
-    // it(msg, async () => {
-    //   validatePathParametersResult = await validatePathParameters(OK_CODE, OK_CODE_DETAILS);
-    //   expect(typeof validatePathParametersResult == "object").toBe(true);
-
-    //   expect(validatePathParametersResult.statusCode == OK_CODE).toBe(true);
-
-    //   let bodyConversion = JSON.parse(validatePathParametersResult.body);
-
-    //   let bodyConversionMessage = JSON.stringify(bodyConversion.message);
-
-    //   expect(bodyConversionMessage == JSON.stringify(OK_CODE_DETAILS)).toBe(
-    //     true
-    //   );
-    // });
-
-    // msg =
-    //   "Should return an object with the value of the statusCode parameter of type any (Number or String)";
-
-    // it(msg, async () => {
-    //   validatePathParametersResult = await validatePathParameters(
-    //     BAD_REQUEST_CODE,
-    //     BAD_REQUEST_CODE_DETAILS
-    //   );
-
-    //   expect(typeof validatePathParametersResult == "object").toBe(true);
-
-    //   expect(
-    //     typeof validatePathParametersResult.statusCode == "number" ||
-    //       typeof validatePathParametersResult.statusCode == "string"
-    //   ).toBe(true);
-    // });
-
-    // msg =
-    //   "Should return an object with the value of the body parameter of type string";
-
-    // it(msg, async () => {
-    //   validatePathParametersResult = await validatePathParameters(
-    //     BAD_REQUEST_CODE,
-    //     BAD_REQUEST_CODE_DETAILS
-    //   );
-
-    //   expect(typeof validatePathParametersResult == "object").toBe(true);
-
-    //   expect(typeof validatePathParametersResult.body == "string").toBe(true);
-    // });
-
-    // msg = "Should return an object if no parameters are passed.";
-
-    // it(msg, async () => {
-    //   validatePathParametersResult = await validatePathParameters();
-
-    //   expect(typeof validatePathParametersResult == "object").toBe(true);
-    // });
-
-    // msg =
-    //   "Should return an object with the value undefined for statusCode and '{}' for body if no parameters are passed.";
-
-    // it(msg, async () => {
-    //   validatePathParametersResult = await validatePathParameters();
-
-    //   expect(typeof validatePathParametersResult == "object").toBe(true);
-
-    //   expect(validatePathParametersResult.statusCode == undefined).toBe(true);
-
-    //   expect(validatePathParametersResult.body == "{}").toBe(true);
-    // });
-
-    // msg =
-    //   "Should return an object with the value null for statusCode if null is passed as a parameter for statusCode argument.";
-
-    // it(msg, async () => {
-    //   validatePathParametersResult = await validatePathParameters(null);
-
-    //   expect(typeof validatePathParametersResult == "object").toBe(true);
-
-    //   expect(validatePathParametersResult.statusCode == null).toBe(true);
-    // });
-
-    // msg =
-    //   "Should return an object with string format for body if null is passed as a parameter for message argument.";
-
-    // it(msg, async () => {
-    //   validatePathParametersResult = await validatePathParameters(BAD_REQUEST_CODE, null);
-
-    //   console.log(typeof validatePathParametersResult.body);
-
-    //   expect(typeof validatePathParametersResult == "object").toBe(true);
-
-    //   expect(typeof validatePathParametersResult.body == "string").toBe(true);
-    // });
-
-    // msg =
-    //   "Should return an object with the value undefined for statusCode if a value is not passed as a parameter for statusCode argument.";
-
-    // it(msg, async () => {
-    //   validatePathParametersResult = await validatePathParameters();
-
-    //   expect(typeof validatePathParametersResult == "object").toBe(true);
-
-    //   expect(validatePathParametersResult.statusCode == undefined).toBe(true);
-    // });
+    msg =
+      "Should return a boolean value if a undefined argument is passed ( This function expects one argument of type object ).";
+    it(msg, async () => {
+      validatePathParametersResult = await validatePathParameters(undefined);
+      await expect(typeof validatePathParametersResult == "boolean").toBe(true);
+    });
   });
 
-//   describe("2) Check cases for error.", () => {
-//     msg = "Should return a boolean with value false if an new Error is passed";
-//     it(msg, async () => {
-//       await expect(async () => await validatePathParameters(new Error())).not.toThrow(
-//         Error
-//       );
-//     });
-//   });
+  describe("2) Check cases for error.", () => {
+    msg = "Should not thrown an Error if a new Error is passed for arguments.";
+    it(msg, async () => {
+      let newError = new Error();
+      validatePathParametersResult = await validatePathParameters(newError);
+      await expect(async () => validatePathParametersResult).not.toThrow(Error);
+    });
+
+    msg =
+      "Should not thrown an Error if no arguments is passed to the function.";
+    it(msg, async () => {
+      validatePathParametersResult = await validatePathParameters();
+      await expect(async () => validatePathParametersResult).not.toThrow(Error);
+    });
+  });
 });
